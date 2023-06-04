4b652035b9aea0a7fa770be3ad879852bdc9438c
CV <brunchwithyou@gmail.com>
2023-06-03 22:48:56 -0700

    Added favicon, logo, fonts. Added initial title, description and animations for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+        fadeOut: "fadeOut 8s ease-out",
+        loader: "loader 0.6s infinite alternate",
+        slideUp: "slideUp 1s ease",
+        slideDown: "slideDown 1s ease",
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { opacity: 0, transform: "translateY(-100%)", visibility: "hidden" },
+          "100%": { opacity: 1, transform: "translateY(0)", visibility: "visible" },
+        },
+        slideUp: {
+          "0%": { opacity: 0, transform: "translateY(100%)", visibility: "hidden" },
+          "100%": { opacity: 1, transform: "translateY(0)", visibility: "visible" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "50%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+        loader: {
+          to: {
+            opacity: 0.1,
+            transform: "translate3d(0, -1rem, 0)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
+};
